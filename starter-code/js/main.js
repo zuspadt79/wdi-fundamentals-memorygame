f8e951b80ebb1df987748ddfa88d164bfd2366e7
zuspadt <samuel.hey@gmail.com>
2016-07-20 13:30:57 +0100

    created game logic

---
+++ starter-code/js/main.js
@@ -0,0 +1,16 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("hello");
+
+
+// if cards match or not, then console.log appropiate message
+if (cardOne === cardFour) {
+	console.log("You've found a match!")
+}else{
+	console.log("Try again!")
+};
+
+
