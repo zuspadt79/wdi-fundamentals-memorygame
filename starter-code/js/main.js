b1a92b6a79bee03682b9ca987df76e78b70e03f3
zuspadt <samuel.hey@gmail.com>
2016-07-27 11:01:40 +0100

    unit 10 assignment

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -3,15 +3,29 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log("hello");
 
+// // if cards match or not, then console.log appropriate message
+// if (cardOne === cardFour) {
+// 	alert("You've found a match!")
+// } 
+// else{
+// 	alert("Try again!")
+// };
 
-// if cards match or not, then console.log appropiate message
-if (cardOne === cardFour) {
-	alert("You've found a match!")
-} 
-else{
-	alert("Try again!")
+var createBoard = function() {
+
+	var gameBoard = document.getElementById('game-board');
+
+	var cards = 4;
+
+	for (var i = 0; i < cards; i++) {
+		var cardDiv = document.createElement('div');
+		cardDiv.setAttribute('class', 'card');
+		gameBoard.appendChild(cardDiv);
+
+		}
 };
 
+// document.getElementById('game-board').addEventListener("click", createBoard);
 
+createBoard();
