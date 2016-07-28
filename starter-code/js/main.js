965c5915eaaee2eb591202a0ed62ebc64012c23b
zuspadt <samuel.hey@gmail.com>
2016-07-28 19:44:17 +0100

    finished WDI fundamentals project

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,31 +1,48 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 
-// // if cards match or not, then console.log appropriate message
-// if (cardOne === cardFour) {
-// 	alert("You've found a match!")
-// } 
-// else{
-// 	alert("Try again!")
-// };
+var gameBoard = document.getElementById('game-board');
 
 var createBoard = function() {
 
-	var gameBoard = document.getElementById('game-board');
-
-	var cards = 4;
-
-	for (var i = 0; i < cards; i++) {
+	for (var i = 0; i < cards.length; i++) {
 		var cardDiv = document.createElement('div');
-		cardDiv.setAttribute('class', 'card');
-		gameBoard.appendChild(cardDiv);
 
+		cardDiv.className = 'card';
+		cardDiv.setAttribute('data-card', cards[i]);
+		cardDiv.addEventListener('click', isTwoCards);
+
+		gameBoard.appendChild(cardDiv);
 		}
 };
 
-// document.getElementById('game-board').addEventListener("click", createBoard);
+var isMatch = function(card) {
+	if (card[0] === card[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Try again!");
+	}
+
+	for (var i = 0; i < cards.length; i++) {
+		document.getElementsByClassName('card')[i].innerHTML = " ";
+	}
+}
+
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='/Users/zuspadt/fundamentals/wdi-fundamentals-memorygame/starter-code/images/king_of_spades.png'' alt='King of Spades'/>";
+	} else {
+		this.innerHTML = "<img src='/Users/zuspadt/fundamentals/wdi-fundamentals-memorygame/starter-code/images/queen_of_hearts.png' alt='Queen of Hearts'/>";
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
 
 createBoard();
